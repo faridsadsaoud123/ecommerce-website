d4d84d131f5201a127f77626e6c744ff7d9edd4b
Farid SAD SAOUD <faridsadsaoud56@gmail.com>
2024-08-17 14:28:23 +0200

    setting tailwind and bootstrap

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+function App() {
+  return (
+    <div className="bg-red-500">
+    <Header />
+      hello
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
